79a421866afc4e9b635490483969219257403407
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2026-01-15 19:27:20 +0300

    feat: implement splash screen functionality with background color, primary text color, and structured layout for improved user interface

--- lib/app/views/view_splash/splash_view.dart
+++ lib/app/views/view_splash/splash_view.dart
@@ -1,3 +1,9 @@
+import 'package:announce_app/app/constant/color_constant.dart';
+import 'package:announce_app/app/constant/spacing_constant.dart';
+import 'package:announce_app/app/views/view_splash/widgets/spash_logo_widget.dart';
+import 'package:announce_app/app/views/view_splash/widgets/spash_title_widget.dart';
+import 'package:announce_app/app/views/view_splash/widgets/splash_loading_widget.dart';
+import 'package:announce_app/app/views/view_splash/widgets/splash_slogan_widget.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -9,10 +15,37 @@ class SplashView extends StatefulWidget {
 
 class _SplashViewState extends State<SplashView> {
   @override
+  void initState() {
+    super.initState();
+    // App launch simulation (redirects to home page after 2.5 seconds)
+    Future.delayed(const Duration(milliseconds: 2500), () {
+      // Navigator.pushReplacementNamed(context, '/home'); // You can write your route here
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Splash'),
+    final bgColor = AppColors.getBackgroundColor(context);
+    final primaryTextColor = AppColors.getTextPrimaryColor(context);
+
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SplashLogoWidget(),
+                AppSizedBox.height24,
+                SplashTitleWidget(primaryTextColor),
+                AppSizedBox.height8,
+                SpashSloganWidget(),
+              ],
+            ),
+          ),
+          SpashLoadingWidget(),
+        ],
       ),
     );
   }
