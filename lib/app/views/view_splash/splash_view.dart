b54977166b03d3fda5038f68bc1ad57ce72df4c7
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2026-01-17 09:16:24 +0300

    feat: update splash screen to redirect to onboarding view after launch simulation for improved user flow

--- lib/app/views/view_splash/splash_view.dart
+++ lib/app/views/view_splash/splash_view.dart
@@ -1,5 +1,6 @@
 import 'package:announce_app/app/constant/color_constant.dart';
 import 'package:announce_app/app/constant/spacing_constant.dart';
+import 'package:announce_app/app/views/view_onboarding/onboarding_view.dart';
 import 'package:announce_app/app/views/view_splash/widgets/spash_logo_widget.dart';
 import 'package:announce_app/app/views/view_splash/widgets/spash_title_widget.dart';
 import 'package:announce_app/app/views/view_splash/widgets/splash_loading_widget.dart';
@@ -17,9 +18,14 @@ class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
     super.initState();
-    // App launch simulation (redirects to home page after 2.5 seconds)
+    // App launch simulation (redirects to onboarding page after 2.5 seconds)
     Future.delayed(const Duration(milliseconds: 2500), () {
-      // Navigator.pushReplacementNamed(context, '/home'); // You can write your route here
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const OnboardingView()),
+        );
+      }
     });
   }
 
